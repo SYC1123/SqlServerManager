01af43c28e6176b090ce7986c760e8ac7f605e97
syc1123 <874795069@qq.com>
2019-11-08 23:54:15 +0800

    增加excel表格功能

--- first.py
+++ first.py
@@ -5,13 +5,8 @@
 # Created by: PyQt5 UI code generator 5.13.0
 #
 # WARNING! All changes made in this file will be lost!
-import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from socket import *
-import pymssql
-from PyQt5.QtWidgets import QMessageBox, QApplication
-import aas
 
 
 class Ui_Dialog(object):
@@ -54,13 +49,6 @@ class Ui_Dialog(object):
         self.label_4.setGeometry(QtCore.QRect(60, 150, 441, 21))
         self.label_4.setObjectName("label_4")
 
-        # 根据序列号查询
-        self.pushButton.clicked.connect(lambda: self.findByID(self.lineEdit))
-        # 根据日期查询
-        self.pushButton_2.clicked.connect(lambda: self.findByDate(self.lineEdit_2))
-        # 发送序列号工作
-        self.pushButton_3.clicked.connect(lambda: self.workByID(self.textEdit))
-
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
@@ -74,66 +62,3 @@ class Ui_Dialog(object):
         self.label_2.setText(_translate("Dialog", "日期："))
         self.label_3.setText(_translate("Dialog", "工作序列号："))
         self.label_4.setText(_translate("Dialog", "请按照年-月-日格式输入。例如2019-02-03"))
-
-    def findByID(self, textfile):
-        data = textfile.text()
-        if data == "":
-            a = QMessageBox.warning(self.pushButton, "警告", "序列号不能为空！", QMessageBox.Yes)
-        else:
-            data = textfile.text()
-            connect = pymssql.connect('(local)', 'sa', '123456', 'lCS_Common')  # 服务器名,账户,密码,数据库名
-            if connect:
-                print("连接成功!")
-            cursor = connect.cursor()  # 创建一个游标对象,python里的sql语句都要通过cursor来执行
-            sql = "SELECT * FROM tbl_EOR_Data WHERE id= %s" % (data)
-            cursor.execute(sql)  # 执行sql语句
-            row = cursor.fetchone()  # 读取查询结果
-            if row == None:
-                a = QMessageBox.warning(self.pushButton, "警告", "不存在该记录！", QMessageBox.Yes)
-            else:
-                while row:  # 循环读取所有结果
-                    print("ID=%s, Date=%s" % (row[0], row[1]))  # 输出结果
-                    row = cursor.fetchone()
-            cursor.close()
-            connect.close()
-
-    def findByDate(self, textfile):
-        data = textfile.text()
-        if data == "":
-            a = QMessageBox.warning(self.pushButton, "警告", "日期不能为空！", QMessageBox.Yes)
-        else:
-            connect = pymssql.connect('(local)', 'sa', '123456', 'lCS_Common')  # 服务器名,账户,密码,数据库名
-            if connect:
-                print("连接成功!")
-            cursor = connect.cursor()  # 创建一个游标对象,python里的sql语句都要通过cursor来执行
-            sql = "SELECT * FROM tbl_EOR_Data WHERE Convert(varchar,date,120) LIKE %s" % (data)
-            sql = sql + '%'
-            sql1 = sql[0:sql.rfind(' ')]
-            sql2 = sql[sql.rfind(' ') + 1:]
-            finalsql = sql1 + "'" + sql2 + "'"
-            cursor.execute(finalsql)  # 执行sql语句
-            row = cursor.fetchone()  # 读取查询结果,
-            if row == None:
-                a = QMessageBox.warning(self.pushButton, "警告", "不存在该记录！", QMessageBox.Yes)
-            else:
-                while row:  # 循环读取所有结果
-                    print("ID=%s, Date=%s" % (row[0], row[1]))  # 输出结果
-                    row = cursor.fetchone()
-            cursor.close()
-            connect.close()
-
-    def workByID(self, texfile):
-        try:
-            HOST = '127.0.0.1'  # 服务端ip
-            PORT = 21566  # 服务端端口号
-            BUFSIZ = 1024
-            ADDR = (HOST, PORT)
-            tcpCliSock = socket(AF_INET, SOCK_STREAM)  # 创建socket对象
-            tcpCliSock.connect(ADDR)  # 连接服务器
-            data = texfile.toPlainText()
-            tcpCliSock.send(data.encode('utf-8'))  # 发送消息
-            data = tcpCliSock.recv(BUFSIZ)  # 读取消息
-            print(data.decode('utf-8'))
-            tcpCliSock.close()  # 关闭客户端
-        except Exception:
-            a = QMessageBox.warning(self.pushButton, "警告", "控制器未打开！", QMessageBox.Yes)
--- test.py
+++ test.py
@@ -1,6 +1,143 @@
+import os
 import sys
 import first
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from socket import *
+import pymssql
+import time
+import xlwt
+
+
+# 设置表格样式
+def set_style(name, height, bold=False):
+    style = xlwt.XFStyle()
+    font = xlwt.Font()
+    font.name = name
+    font.bold = bold
+    font.color_index = 4
+    font.height = height
+    style.font = font
+    return style
+
+
+def findByID(button, textfile):
+    data = textfile.text()
+    if data == "":
+        a = QMessageBox.warning(button, "警告", "序列号不能为空！", QMessageBox.Yes)
+    else:
+        data = textfile.text()
+        connect = pymssql.connect('(local)', 'sa', '123456', 'lCS_Common')  # 服务器名,账户,密码,数据库名
+        if connect:
+            print("连接成功!")
+        cursor = connect.cursor()  # 创建一个游标对象,python里的sql语句都要通过cursor来执行
+        sql1 = "select name from syscolumns where id = object_id('tbl_EOR_Data')"
+        cursor.execute(sql1)  # 执行sql语句
+        row = cursor.fetchone()  # 读取查询结果
+        list = []
+        while row:  # 循环读取所有结果
+            list.append(row[0])
+            row = cursor.fetchone()
+        new_list = []
+        for i in range(len(list)):
+            a = list.pop()
+            new_list.append(a)
+
+        sql = "SELECT * FROM tbl_EOR_Data WHERE id= %s" % (data)
+        cursor.execute(sql)  # 执行sql语句
+        row = cursor.fetchone()  # 读取查询结果
+        if row == None:
+            a = QMessageBox.warning(button, "警告", "不存在该记录！", QMessageBox.Yes)
+        else:
+            count = 1
+            f = xlwt.Workbook()
+            sheet1 = f.add_sheet('序列号找到的工件', cell_overwrite_ok=True)
+            while row:  # 循环读取所有结果
+                # 写第一行
+                for i in range(0, len(new_list)):
+                    sheet1.write(0, i, new_list[i], set_style('Times New Roman', 220, True))  # 行 列
+                # 写一行数据
+                for a in range(0, len(new_list)):
+                    sheet1.col(a).width = 256 * 20
+                    sheet1.write(count, a, str(row[a]))
+                count = count + 1
+                row = cursor.fetchone()
+        ticks = time.time()
+        f.save(os.path.join(os.path.expanduser('~'), "Desktop") + "\\" + str(ticks) + "序列号.xls")
+        cursor.close()
+        connect.close()
+
+
+def findByDate(button, textfile):
+    global f1
+    data = textfile.text()
+    if data == "":
+        a = QMessageBox.warning(button, "警告", "日期不能为空！", QMessageBox.Yes)
+    else:
+        connect = pymssql.connect('(local)', 'sa', '123456', 'lCS_Common')  # 服务器名,账户,密码,数据库名
+        if connect:
+            print("连接成功!")
+        cursor = connect.cursor()  # 创建一个游标对象,python里的sql语句都要通过cursor来执行
+        sql1 = "select name from syscolumns where id = object_id('tbl_EOR_Data')"
+        cursor.execute(sql1)  # 执行sql语句
+        row = cursor.fetchone()  # 读取查询结果
+        list = []
+        while row:  # 循环读取所有结果
+            list.append(row[0])
+            row = cursor.fetchone()
+        new_list = []
+        for i in range(len(list)):
+            a = list.pop()
+            new_list.append(a)
+
+        sql = "SELECT * FROM tbl_EOR_Data WHERE Convert(varchar,date,120) LIKE %s" % (data)
+        sql = sql + '%'
+        sql1 = sql[0:sql.rfind(' ')]
+        sql2 = sql[sql.rfind(' ') + 1:]
+        finalsql = sql1 + "'" + sql2 + "'"
+        cursor.execute(finalsql)  # 执行sql语句
+        row = cursor.fetchone()  # 读取查询结果,
+        if row == None:
+            a = QMessageBox.warning(button, "警告", "不存在该记录！", QMessageBox.Yes)
+        else:
+            co = 1
+            f1 = xlwt.Workbook()
+            sheet = f1.add_sheet('日期找到的工件', cell_overwrite_ok=True)
+            while row:  # 循环读取所有结果
+                # 写第一行
+                for i in range(0, len(new_list)):
+                    sheet.write(0, i, new_list[i], set_style('Times New Roman', 220, True))  # 行 列
+                # 写一行数据
+                for a in range(0, len(new_list)):
+                    sheet.col(a).width = 256 * 20
+                    sheet.write(co, a, str(row[a]))
+                co = co + 1
+                print()
+                row = cursor.fetchone()
+        ticks = time.time()
+        f1.save(os.path.join(os.path.expanduser('~'), "Desktop") + "\\" + str(ticks) + "日期.xls")
+        cursor.close()
+        connect.close()
+
+
+def workByID(button, texfile):
+    if texfile.toPlainText() == '':
+        a = QMessageBox.warning(button, "警告", "序列号不能为空！", QMessageBox.Yes)
+    else:
+        try:
+            HOST = '127.0.0.1'  # 服务端ip
+            PORT = 21566  # 服务端端口号
+            BUFSIZ = 1024
+            ADDR = (HOST, PORT)
+            tcpCliSock = socket(AF_INET, SOCK_STREAM)  # 创建socket对象
+            tcpCliSock.connect(ADDR)  # 连接服务器
+            data = texfile.toPlainText()
+            tcpCliSock.send(data.encode('utf-8'))  # 发送消息
+            data = tcpCliSock.recv(BUFSIZ)  # 读取消息
+            print(data.decode('utf-8'))
+            tcpCliSock.close()  # 关闭客户端
+        except Exception:
+            a = QMessageBox.warning(button, "警告", "控制器未打开！", QMessageBox.Yes)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -8,4 +145,10 @@ if __name__ == '__main__':
     ui = first.Ui_Dialog()
     ui.setupUi(mainWindow)
     mainWindow.show()
+    # 根据序列号查询
+    ui.pushButton.clicked.connect(lambda: findByID(ui.pushButton, ui.lineEdit))
+    # 根据日期查询
+    ui.pushButton_2.clicked.connect(lambda: findByDate(ui.pushButton_2, ui.lineEdit_2))
+    # 发送序列号工作
+    ui.pushButton_3.clicked.connect(lambda: workByID(ui.pushButton_3, ui.textEdit))
     sys.exit(app.exec())
